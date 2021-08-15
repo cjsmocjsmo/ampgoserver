f5e2f7ddd2cae6b6623349f362ddcd7d55237877
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-08-15 12:59:49 -0700

    bug fix

--- ampgoserver.go
+++ ampgoserver.go
@@ -206,7 +206,6 @@ func albumsForArtistHandler(w http.ResponseWriter, r *http.Request) {
 	cur, err := coll.Find(context.TODO(), filter, opts)
 	ServerCheckError(err, "albumsForArtistHandler has failed")
 	// var tv []map[string]string
-
 	var tv [][]map[string]string
 
 	if err = cur.All(context.TODO(), &tv); err != nil {
