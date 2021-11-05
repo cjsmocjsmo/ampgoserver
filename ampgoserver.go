c025b1d252a67a4f561bcdbd51522f347891f09c
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-11-04 23:48:04 -0700

    fixed artist

--- ampgoserver.go
+++ ampgoserver.go
@@ -184,7 +184,7 @@ func artistInfoByPageHandler(w http.ResponseWriter, r *http.Request) {
 	filter := bson.M{"page": page}
 	opts := options.Find()
 	// opts.SetLimit(int64(limit))
-	opts.SetProjection(bson.M{"_id": 0, "artist": 1, "artistID": 1, "albcount": 1})
+	opts.SetProjection(bson.M{"_id": 0})
 	client, ctx, cancel, err := ampgosetup.Connect("mongodb://db:27017/ampgodb")
 	defer ampgosetup.Close(client, ctx, cancel)
 	ServerCheckError(err, "MongoDB connection has failed")
